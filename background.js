5d184ba78f742b2f8271433b6cea6143b3074352
Kevin Li <kevin.li@checkout.com>
2025-01-11 23:41:41 +0000

    right click to rename

--- background.js
+++ background.js
@@ -1,3 +1,36 @@
+// Create context menu when extension is installed
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'renameTab',
+    title: 'Rename Tab',
+    type: 'normal',
+    contexts: ['page'],
+    documentUrlPatterns: ['<all_urls>']
+  });
+});
+
+// Handle context menu clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'renameTab') {
+    const newName = prompt('Enter new name for tab:', tab.title);
+    if (newName) {
+      // Store the new name
+      const data = {};
+      data[tab.url] = newName;
+      chrome.storage.local.set(data, function() {
+        // Update the tab title
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: (newTitle) => {
+            document.title = newTitle;
+          },
+          args: [newName]
+        }).catch(err => console.log('Script injection failed:', err));
+      });
+    }
+  }
+});
+
 // Handle tab updates
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
